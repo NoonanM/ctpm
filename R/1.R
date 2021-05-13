6ea620c06ffc9fae3d41adb70cb8ab8297d36053
Michael Noonan <michaelnoonan@Michaels-iMac.local>
2021-05-13 14:41:20 -0700

    Typo

--- R/1.R
+++ R/1.R
@@ -1,7 +1,7 @@
 # Universal stuff that needs to be run first
 
 methods::setOldClass("variogram")
-new.variogram <- methods::setClass("variogram",slots = c(Distance="numeric",
-                                                         Gamma="numeric",
-                                                         CI_min="numeric",
-                                                         CI_max="numeric"))
+new.variogram <- methods::setClass("variogram",representation(Distance="numeric",
+                                                              Gamma="numeric",
+                                                              CI_min="numeric",
+                                                              CI_max="numeric"))
