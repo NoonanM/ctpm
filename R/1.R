764545a57358080a462db9166ed03a8167cfe3c4
Michael Noonan <michaelnoonan@Michaels-iMac.local>
2021-05-14 18:17:06 -0700

    bug fix

--- R/1.R
+++ R/1.R
@@ -1,4 +1,8 @@
 # Universal stuff that needs to be run first
+methods::setOldClass("UERE")
+new.UERE <- methods::setClass("UERE",contains="list",representation=methods::representation(info="list"),
+                              prototype=methods::prototype(list(UERE=cbind(0),DOF=cbind(0),AICc=Inf,Zsq=Inf,VAR.Zsq=Inf,N=0),info=list()))
+
 methods::setOldClass("variogram")
 new.variogram <- methods::setClass("variogram",representation=methods::representation("data.frame",info="list",UERE="UERE"),
                                    prototype=methods::prototype(data.frame(),info=list(),UERE=new.UERE()) )
