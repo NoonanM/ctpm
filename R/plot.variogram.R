5a492283e389e4ed8c08255cf85afda881c5bd9f
Michael Noonan <michaelnoonan@Michaels-iMac.local>
2021-05-18 11:17:22 -0700

    Typo

--- R/plot.variogram.R
+++ R/plot.variogram.R
@@ -4,7 +4,8 @@
 
 plot.variogram <- function(x, CTPM = NULL, col="black", col.CTPM = "red", fraction = 1, y.units = NULL, ...){
   
-  suppressWarnings(ctmm:::plot.variogram(SVF,
+  
+  suppressWarnings(ctmm:::plot.variogram(x,
                         CTMM = CTPM,
                         col.CTMM = col.CTPM,
                         fraction = fraction,
